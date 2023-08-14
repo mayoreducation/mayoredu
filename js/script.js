59c43c6c9cba7249195a33fbb0aa5424b1aa43db
mayoreducation <91301869+mayoreducation@users.noreply.github.com>
2023-08-14 12:02:23 +0545

    update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 (function ($) {
 	'use strict';
 
-	// Preloader js    
+	// Preloader js
 	$(window).on('load', function () {
 		$('.preloader').fadeOut(700);
 	});
+
+
 
 	// Sticky Menu
 	$(window).scroll(function () {
